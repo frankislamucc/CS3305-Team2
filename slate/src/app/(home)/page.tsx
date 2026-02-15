0f117f177292b4f883b731710d8e8fbc8ee8b88e
naoiseosullivan <123400226@umail.ucc.ie>
2026-02-15 15:56:07 +0000

    add footer and background

--- slate/src/app/(home)/page.tsx
+++ slate/src/app/(home)/page.tsx
@@ -3,6 +3,8 @@
 import HomeNavbar from "./_components/ui/HomeNavbar";
 import BentoCards from "./_components/ui/BentoCards";
 import { useEffect, useRef, useState } from "react";
+import ColorBends from "@/components/ui/ColorBends";
+import Footer from "./_components/ui/Footer";
 
 export default function HomePage() {
   const [navHeight, setNavHeight] = useState<number>(0);
@@ -17,66 +19,25 @@ export default function HomePage() {
 
   // TODO: is state needed, can we just use turnary with navRef.current : 0
   return (
-    <div>
+    <div className="relative min-h-screen w-full">
       <HomeNavbar navRef={navRef} />
       <div style={{ height: navHeight }} aria-hidden="true" />
+      <ColorBends
+        className="fixed inset-0 -z-10"
+        colors={["#ff5c7a", "#8a5cff", "#00ffd1"]}
+        rotation={0}
+        speed={0.2}
+        scale={1}
+        frequency={1}
+        warpStrength={1}
+        mouseInfluence={1}
+        parallax={0.5}
+        noise={0.1}
+        transparent
+        autoRotate={0}
+      />
       <BentoCards />
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
-      <p>this word</p>
+      <Footer />
     </div>
   );
 }
