3cc07d87fcdd9afa3a8121b3cc7d5b92d7d925ac
frankislamucc <123366953@umail.ucc.ie>
2026-03-06 16:09:06 +0000

    redirects user directly to the home page after registering instead of them having to login

--- slate/src/app/(auth)/actions/register.ts
+++ slate/src/app/(auth)/actions/register.ts
@@ -7,6 +7,8 @@ import { redirect } from "next/navigation";
 import { AuthError } from "@/app/(workspace)/whiteboard/_types";
 import schema from "@/lib/validation/auth";
 import z from "zod";
+import { createToken } from "@/lib/auth";
+import { cookies } from "next/headers";
 
 export async function registerAction(
   prevResponse: AuthError | undefined,
@@ -43,14 +45,28 @@ export async function registerAction(
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    await User.create({
+    const user = await User.create({
       username,
       password: hashedPassword,
     });
+
+    const token = await createToken({
+      userId: user._id.toString(),
+      username: user.username,
+    });
+
+    const cookieStore = await cookies();
+
+    cookieStore.set("session", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 604800, // 7 days
+      path: "/",
+    });
   } catch (error: unknown) {
     console.log(error);
-    return { errorMessages: ["Uknown error occurred, please try again"] };
+    return { errorMessages: ["Unknown error occurred, please try again"] };
   }
-
-  redirect("/login");
+  redirect("/");
 }
